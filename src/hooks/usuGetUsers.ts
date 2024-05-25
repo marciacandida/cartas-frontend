3948cedbf609d681d65631043bc316c41d5f78ac
Thinker-Dev <107056866+Thinker-Dev@users.noreply.github.com>
2024-05-25 22:22:17 +0200

    commit

--- src/hooks/usuGetUsers.ts
+++ src/hooks/usuGetUsers.ts
@@ -8,14 +8,12 @@ interface IGetUser {
 
 const useGetUsers = ({ query }: IGetUser) => {
   const [users, setUsers] = useState<IUser[]>([]);
-  console.log("we here", query )
+
   useEffect(() => {
     const fetchUsers = async () => {
       await axiosInstance
         .get("/getUsers", {
-          params: {
-            query: query ? query : null,
-          },
+    
         })
         .then((res) => {setUsers(res.data); console.log("res",res)})
         .catch((err) => console.error(err));
